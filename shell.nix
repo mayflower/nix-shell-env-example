322ee80cff5b69adfd852bac0f9556778102a820
Robin Gloster <mail@glob.in>
2018-03-24 11:45:47 +0100

    Pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,12 @@
-with import <nixpkgs> {};
+let
+  fetchNixpkgs = import ./fetchNixpkgs.nix;
+  rev = "0e7c9b32817e5cbe61212d47a6cf9bcd71789322"; # 18.03-beta as of 22-Mar-2018
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    sha256 = "1dm777cmlhqcwlrq8zl9q2d87h3p70rclpvq36y43kp378f3pd0y";
+  };
+in
+with import nixpkgs { config = {}; overlays = []; };
 
 stdenv.mkDerivation {
   name = "test-project";
